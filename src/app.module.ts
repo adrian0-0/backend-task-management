d4976e92949cdd9e89989c176604bd1bdc7996d8
Adrian Vinícius Roma Alves <adrianromaalves@hotmail.com>
2024-12-26 20:31:45 -0300

    chore: rm unnused libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskEntity } from './tasks/task.entity';
+import { TaskRepository } from './tasks/tasks.repository';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
